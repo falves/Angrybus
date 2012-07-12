50b76544ccf75fa5b3d0097974bcca133f5b6c65
Felipe Alves <felipe@bitix.com.br>
2012-07-12 15:51:03 -0300

    Adicionado framework de testes

---
+++ TestesAngybus/TestesAngybus.h
@@ -0,0 +1,13 @@
+//
+//  TestesAngybus.h
+//  TestesAngybus
+//
+//  Created by Felipe Alves on 11/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestesAngybus : SenTestCase
+
+@end
---
+++ TestesAngybus/TestesAngybus.m
@@ -0,0 +1,32 @@
+//
+//  TestesAngybus.m
+//  TestesAngybus
+//
+//  Created by Felipe Alves on 11/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestesAngybus.h"
+
+@implementation TestesAngybus
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestesAngybus");
+}
+
+@end
